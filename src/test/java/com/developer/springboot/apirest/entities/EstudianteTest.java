3a73dabaac2607cf43069a8ac4cb48769060c7c0
EXA00696 <cesar.aramayo@dicsys.com>
2023-02-01 18:08:51 -0300

    implementando TDD en la clase Estudiante

--- src/test/java/com/developer/springboot/apirest/entities/EstudianteTest.java
+++ src/test/java/com/developer/springboot/apirest/entities/EstudianteTest.java
@@ -2,13 +2,23 @@ package com.developer.springboot.apirest.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 class EstudianteTest {
 
+	
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testReferenciaEstudiante() {
+		LocalDate fechaFinalizacion = LocalDate.now();
+		Estudiante estudiante = new Estudiante("Fernando", 18, fechaFinalizacion, 3);
+		
+		Estudiante estudiante2 = new Estudiante("Fernando", 18, fechaFinalizacion, 3);
+		
+		assertEquals(estudiante2, estudiante);
+		
 	}
 
 }
